f47b70e786dd8fb17f7ce97bc8f6383b4d901eba
Sambi <sambisingh289@gmail.com>
2024-10-29 13:55:48 -0500

    Added a java script file to show results from clicking on search bar (GOTTA ADD TO IT)

---
+++ searchBar.js
@@ -0,0 +1,18 @@
+
+//Need to fix this method
+function search()
+{
+    let input = document.getElementById('search_bar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName(''); 
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "list-item";
+        }
+    }
+
+}
